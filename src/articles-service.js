2388fac8624f8d575def9f2454ebf538df4ff78d
Kim McCallum <kchaase@gmail.com>
2020-03-31 17:15:31 -0600

    get help with insert method

--- src/articles-service.js
+++ src/articles-service.js
@@ -1,7 +1,13 @@
 const ArticlesService = {
     getAllArticles(knex){
-        return knex.select('*').from('blogful-articles')
-    }
+        return knex.select('*').from('blogful_articles')
+    },
+    insertArticle(knex, newArticle){
+        return knex
+            .insert(newArticle)
+            .into('blogful_articles')
+            .returning('*')
+    },
 }
 
 module.exports = ArticlesService;
--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -5,14 +5,20 @@ describe(`Articles service object`, function(){
     let db; 
     let testArticles = [ 
         {
+            id: 1,
+            date_published: new Date('2029-01-22T16:28:32.615Z'),
             title: "Frist test post!",
             content: "Lorem ipsum yada yadaLorem amet irure eu velit sit sit."
         },
         {
+            id: 2,
+            date_published: new Date('2029-02-22T16:28:32.615Z'),
             title:'Second test post!',
             content: 'Laborum nisi et id Lorem deserunt dolor anim dolore.'
         },
         {
+            id: 3,
+            date_published: new Date('2079-02-22T16:28:32.615Z'),
             title:'Third test post!',
             content:'Eiusmod ullamco sit nulla incididunt ad ex veniam aute id officia laborum adipisicing.'
         },
@@ -27,19 +33,50 @@ describe(`Articles service object`, function(){
 
     before(() => db('blogful_articles').truncate())
 
-    before(() => {
-        return db 
-            .into('blogful_articles')
-            .insert(testArticles)
-    })
+    this.afterEach(() => db('blogful_articles').truncate())
 
     after(() => db.destroy())
 
-    describe(`getAllArticles()`, () => { 
-        it(`resolves all articles from 'blogful_articles' table`, () => {
+    context(`Given 'blogful_articles' has data`, () => {
+        before(() => {
+            return db 
+                .into('blogful_articles')
+                .insert(testArticles)
+        })
+
+        it(`getAllArticles() resolves all articles from 'blogul_articles' table`, () => {
             return ArticlesService.getAllArticles(db)
                 .then(actual => {
-                    expect(actual).to.eql(testArticles)
+                   expect(actual).to.eql(testArticles.map(article => ({
+                        ...article,
+                        date_published: new Date(article.date_published)
+                    })))
+                })
+        })
+    })
+
+    context(`Given 'blogful_articles' has no data`, () => {
+        it(`getAllArticles() resolves an empty array`, () => {
+            return ArticlesService.getAllArticles(db)
+                .then(actual => {
+                    expect(actual).to.eql([])
+                })
+        })
+
+        it(`insertArticle() inserts a new article and resolves the new article with an 'id'`, () => {
+            const newArticle = {
+                title: 'Test new title',
+                content: 'Test new content',
+                date_published: new Date('2020-01-01T00:00:00.000Z')
+            }
+            return ArticlesService.insertArticle(db, newArticle)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        id:1, 
+                        title: newArticle.title,
+                        content: newArticle.content, 
+                        date_published: newArticle.date_published,
+                    })
                 })
         })
     })
