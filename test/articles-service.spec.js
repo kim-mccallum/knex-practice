3e0a744cd8ed86fb80d67be8f9b0d5fe439f5551
Kim McCallum <kchaase@gmail.com>
2020-04-01 10:38:52 -0600

    get help with shopping list test

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -1,7 +1,7 @@
 const ArticlesService = require('../src/articles-service')
 const knex = require('knex')
 
-describe(`Articles service object`, function(){
+describe.skip(`Articles service object`, function(){
     let db; 
     let testArticles = [ 
         {
@@ -49,7 +49,7 @@ describe(`Articles service object`, function(){
                 .then(actual => {
                    expect(actual).to.eql(testArticles.map(article => ({
                         ...article,
-                        date_published: new Date(article.date_published)
+                        date_published: article.date_published
                     })))
                 })
         })
@@ -59,12 +59,41 @@ describe(`Articles service object`, function(){
             const thirdTestArticle = testArticles[thirdId - 1]
             return ArticlesService.getById(db, thirdId)
                 .then(actual => {
-                    console.log(actual)
+                    // console.log(`these match? ${actual.date_published === thirdTestArticle.date_published}`)
+                    // console.log(`these match? ${actual.date_published} and ${thirdTestArticle.date_published}`)
                     expect(actual).to.eql({
                         id: thirdId, 
-                        title: thirdTestArticle, 
+                        title: thirdTestArticle.title, 
                         content: thirdTestArticle.content, 
-                        date_published: thirdTestArticle.date_published,
+                        date_published: thirdTestArticle.date_published
+                    })
+                })
+        })
+
+        it(`deleteArticle() removes an article by id from 'blogful_articles' table`, () => {
+            const articleId = 3
+            return ArticlesService.deleteArticle(db, articleId)
+                //two promises here? Explain
+                .then(() => ArticlesService.getAllArticles(db))
+                .then(allArticles  => {
+                    const expected = testArticles.filter(article => article.id !== articleId)
+                    expect(allArticles).to.eql(expected)
+                })
+        })
+
+        it(`updateArticle() updates an article from the 'blogful_articles' table`, () => {
+            const idOfArticleToUpdate = 3
+            const newArticleData = {
+                title: 'updated title',
+                content: 'updated content',
+                date_published: new Date(),
+            }
+            return ArticlesService.updateArticle(db, idOfArticleToUpdate, newArticleData)
+                .then(()=> ArticlesService.getById(db, idOfArticleToUpdate))
+                .then(article => {
+                    expect(article).to.eql({
+                        id: idOfArticleToUpdate,
+                        ...newArticleData,
                     })
                 })
         })
---
+++ test/shopping-list-service.spec.js
@@ -0,0 +1,142 @@
+const ShoppingListService = require('../src/shopping-list-service')
+const knex = require('knex')
+
+describe(`Shopping list service object`, function(){
+    let db; 
+    let testItems = [ 
+        {
+            id: 1,
+            date_added: new Date('2029-01-22T16:28:32.615Z'),
+            name: "First test item!",
+            price:10,
+            category: "Lunch",
+            checked: false
+        },
+        {
+            id: 2,
+            date_added: new Date('2020-01-22T16:28:32.615Z'),
+            name: "Second test item!",
+            price:1,
+            category: "Lunch",
+            checked: false        
+        },
+        {
+            id: 3,
+            date_added: new Date('2021-01-22T16:28:32.615Z'),
+            name: "Third test item!",
+            price:5,
+            category: "Lunch",
+            checked: false
+        },
+    ]
+
+    before(() => {
+        db = knex({
+            client:'pg',
+            connection: process.env.TEST_DB_URL,
+        })
+    })
+
+    before(() => db('shopping_list').truncate())
+
+    this.afterEach(() => db('shopping_list').truncate())
+
+    after(() => db.destroy())
+
+    context(`Given 'shopping_list' has data`, () => {
+        beforeEach(() => {
+            return db 
+                .into('shopping_list')
+                .insert(testItems)
+        })
+
+        it(`getAllItems() resolves all items from 'shopping_list' table`, () => {
+            return ShoppingListService.getAllItems(db)
+                .then(actual => {
+                   expect(actual).to.eql(testItems.map(item => ({
+                        ...item,
+                        date_added: item.date_added
+                    })))
+                })
+        })
+
+        it(`getById() resolves an Item by id from 'shopping_list' tables`, () =>{
+            const thirdId = 3;
+            const thirdTestItem = testItems[thirdId - 1]
+            return ArticlesService.getById(db, thirdId)
+                .then(actual => {
+                    // console.log(`these match? ${actual.date_added === thirdTestArticle.date_added}`)
+                    // console.log(`these match? ${actual.date_added} and ${thirdTestArticle.date_added}`)
+                    expect(actual).to.eql({
+                        id: thirdId, 
+                        name: thirdTestItem.name, 
+                        category: thirdTestItem.category, 
+                        checked: thirdTestItem.checked,
+                        price: thirdTestItem.price,
+                        date_added: thirdTestItem.date_added
+                    })
+                })
+        })
+
+        it(`deleteItem() removes an Item by id from 'shopping_list' table`, () => {
+            const itemId = 3
+            return ShoppingListService.deleteItem(db, itemId)
+                //two promises here? Explain
+                .then(() => ShoppingListService.getAllItems(db))
+                .then(allItems  => {
+                    const expected = testItems.filter(item => item.id !== itemId)
+                    expect(allItems).to.eql(expected)
+                })
+        })
+
+        it(`updateItem() updates an article from the 'shopping_list' table`, () => {
+            const idOfItemToUpdate = 3
+            const newItemData = {
+                name: 'updated name',
+                category: 'Main',
+                price: 100,
+                checked: true,
+                date_added: new Date(),
+            }
+            return ShoppingListService.updateItem(db, idOfItemToUpdate, newItemData)
+                .then(()=> ShoppingListService.getById(db, idOfItemToUpdate))
+                .then(item => {
+                    expect(item).to.eql({
+                        id: idOfItemToUpdate,
+                        ...newItemData,
+                    })
+                })
+        })
+    })
+
+    context(`Given 'shopping_list' has no data`, () => {
+        it(`getAllItems() resolves an empty array`, () => {
+            return ShoppingListService.getAllItems(db)
+                .then(actual => {
+                    expect(actual).to.eql([])
+                })
+        })
+
+        it(`insertItem() inserts a new item and resolves the new item with an 'id'`, () => {
+            const newItem = {
+                name: 'Test new name',
+                price: 100,
+                category: 'Main',
+                checked:true,
+                date_added: new Date('2020-01-01T00:00:00.000Z')
+            }
+            return ShoppingListService.insertItem(db, newItem)
+                .then(actual => {
+                    expect(actual).to.eql({
+                        // why do I expect the id to be 1?
+                        id:1, 
+                        name: newItem.name,
+                        price: newItem.price,
+                        cheked: newItem.price,
+                        category: newItem.category, 
+                        date_added: new Date (newItem.date_added)
+                    })
+                })
+        })
+    })
+})
